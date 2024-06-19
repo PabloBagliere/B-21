08db74cc3defc354beab9fc387d88c8e1f2c9ebf
Pablo Bagliere <pablo.bagliere2@gmail.com>
2024-06-18 21:46:44 -0300

    add metrics

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/PabloBagliere/B-21/internal/router"
 	"github.com/PabloBagliere/B-21/pkg/config"
 	"github.com/PabloBagliere/B-21/pkg/logger"
+	"github.com/labstack/echo-contrib/echoprometheus"
 	"github.com/labstack/echo/v4"
 	"github.com/rs/zerolog"
 )
@@ -15,8 +16,10 @@ func main() {
 	// HealthCheck(e)
 	// ...
 	e := echo.New()
+	e.Use(echoprometheus.NewMiddleware("MyServiceName"))
 	e.Use(logger.MiddlewareLogger("MyServiceName", zerolog.InfoLevel))
 	router.HealthCheck(e)
+	e.GET("/metrics", echoprometheus.NewHandler())
 	p, err := config.GetConfig()
 	if err != nil {
 		e.Logger.Fatal(err)
